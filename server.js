44984e8f5cd22a1d922c36017b66298d10fbeca8
Jakub Stelmasiak <stelmon3@gmail.com>
2025-03-04 16:28:19 +0100

    add public folder

--- server.js
+++ server.js
@@ -4,36 +4,39 @@ const path = require('path');
 const app = express();
 
 app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
+    res.show = (name) => {
+        res.sendFile(path.join(__dirname, `/views/${name}`));
+    };
+    next();
 });
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', (req, res) => {
-  res.show('index.html');
+    res.show('index.html');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+    res.show('about.html');
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+    res.show('contact.html');
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+    res.show('info.html');
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+    res.show('history.html');
 });
+
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
 });
 
 app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+    console.log('Server is running on port: 8000');
 });
